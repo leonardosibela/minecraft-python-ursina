784945b31b518a8ea119f4d6f1b0cd6b8e8e10da
leonardosibela <leonardo_sibela@hotmail.com>
2021-02-13 17:44:23 -0300

    Adding voxel class

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from ursina import *
 
-app = Ursina()
+class Voxel(Button):
+    def __init__(self, position=(0, 0, 0)):
+        super().__init__(
+            parent=scene,
+            position=position,
+            model='cube',
+            origin_y=0.25,
+            texture='white_cube',
+            color=color.white,
+            highlight_color=color.lime
+        )
 
-app.run()
+
+app = Ursina()
