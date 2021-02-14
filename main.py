add7af00dfce31257a45135d9f6519aa3c56f349
leonardosibela <leonardo_sibela@hotmail.com>
2021-02-14 15:18:12 -0300

    Allowing to place different blocks

--- main.py
+++ main.py
@@ -4,15 +4,35 @@ from ursina.prefabs.first_person_controller import FirstPersonController
 app = Ursina()
 
 grass_texture = load_texture('assets/grass_block.png')
+stone_texture = load_texture('assets/stone_block.png')
+brick_texture = load_texture('assets/brick_block.png')
+dirt_texture = load_texture('assets/dirt_block.png')
+block_picked = grass_texture
+
+
+def update():
+    global block_picked
+    if held_keys['1']:
+        block_picked = grass_texture
+
+    if held_keys['2']:
+        block_picked = stone_texture
+
+    if held_keys['3']:
+        block_picked = brick_texture
+
+    if held_keys['4']:
+        block_picked = dirt_texture
+
 
 class Voxel(Button):
-    def __init__(self, position=(0, 0, 0)):
+    def __init__(self, position=(0, 0, 0), texture=grass_texture):
         super().__init__(
             parent=scene,
             position=position,
             model='assets/block',
             origin_y=0.25,
-            texture=grass_texture,
+            texture=texture,
             color=color.color(0, 0, random.uniform(0.9, 1)),
             highlight_color=color.lime,
             scale=0.5
@@ -21,7 +41,7 @@ class Voxel(Button):
     def input(self, key):
         if self.hovered:
             if key == 'left mouse down':
-                voxel = Voxel(position=self.position + mouse.normal)
+                Voxel(position=self.position + mouse.normal, texture=block_picked)
 
             if key == 'right mouse down':
                 destroy(self)
